28cf644a5f62796566acfb022b36de5edfa8af46
Ibrahim Mohamed <Ibrahim_20210007@fci.helwan.edu.eg>
2026-04-21 05:45:13 +0200

    feat: modify the floor plans tab for the spe appartment

--- src/app/[locale]/specific-type/show3d/page.tsx
+++ src/app/[locale]/specific-type/show3d/page.tsx
@@ -17,7 +17,7 @@ export default function FloorTourPage() {
   }
 
   return (
-    <div className="w-full h-[80%] max-[700px]:h-[78%]  flex items-center">
+    <div className="w-full h-[80%] max-[700px]:h-[78%] flex items-center">
       <iframe src={url} className="w-full h-[90%]" allowFullScreen />
     </div>
   );
